37720792a0195fe6ad4986a59f0d4f9149918995
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2016-09-08 00:14:59 -0300

    fixed index.html

--- www/sarg.php
+++ www/sarg.php
@@ -5,7 +5,7 @@ require("funcoes.php");
 inicia_pagina();
 monta_titulo_secao("Relatórios de Acesso");
 ?>
-<iframe src="index.html" frameborder="0" width="100%" height="500"></iframe>
+<iframe src="sarg.html" frameborder="0" width="100%" height="500"></iframe>
 <?
 require("desconectar_mysql.php");
 termina_pagina();
